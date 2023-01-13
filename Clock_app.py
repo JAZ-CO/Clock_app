a4b5edcb5f63f78139db3b0a43f45f5567897cdc
JAZ-CO <61976148+JAZ-CO@users.noreply.github.com>
2023-01-13 22:19:59 +0300

    UPDATE!

--- Clock_app.py
+++ Clock_app.py
@@ -1,22 +1,26 @@
-import tkinter as tk
-import time
+import tkinter as tk #We'll mostly depend on this
+import time #just to get the time
 
 class GUI:
+
     def __init__(self):
-        self.root = tk.Tk()
+        self.root = tk.Tk() 
         self.root.title("Digital Clock")
 
+        #Label means the text just like paragraph in html
         self.clock = tk.Label(self.root, font=("Calibri", 90), bg="grey", fg="black")
         self.clock.pack()
-        self.get_time()
 
-        self.frame = tk.Frame(self.root)
-        self.frame.columnconfigure(0,weight=1)
-        self.frame.columnconfigure(1, weight=1)
+        self.get_time() #call the function inside class
 
-        self.button1 = tk.Button(self.frame,bg="Black",width=5,height=3,command=(self.make_black))
+        self.frame = tk.Frame(self.root) #this is the frame that the button will go inside it
+        self.frame.columnconfigure(0,weight=1) # I don't know about these
+        self.frame.columnconfigure(0, weight=1)
+
+        #below is buttons
+        self.button1 = tk.Button(self.frame,text="Black",bg="Black",fg="Black",width=5,height=3,command=(self.make_black))
         self.button1.grid(row=0,column=0,sticky="news")
-        self.button2 = tk.Button(self.frame,bg="White",width=5,height=3, command=(self.make_white))
+        self.button2 = tk.Button(self.frame,text="White",bg="White",width=5,height=3, command=(self.make_white))
         self.button2.grid(row=0,column=1,sticky="news")
 
         self.frame.pack(side="bottom")
@@ -27,19 +31,20 @@ class GUI:
 
     def get_time(self):
 
-        self.clock.update()
-        timeVar = time.strftime("%I:%M:%S %p")
-        self.clock.config(text=timeVar)
+        self.clock.update() #update the time
+        timeVar = time.strftime("%I:%M:%S %p")#hr:min:sec am/pm
 
-        self.clock.after(200, self.get_time)
+        self.clock.config(text=timeVar) #this is to show it in window I guess
+
+        self.clock.after(200, self.get_time) #for every 2ms, call the function again
 
 
     def make_black(self):
 
-        self.clock.destroy()
+        self.clock.destroy() #we did this in order not to stack up
         self.clock = tk.Label(self.root, font=("Calibri", 90), bg="Black", fg="White")
-        self.get_time()
-        self.clock.pack(side="top")
+        self.get_time() #This calls the function again so we could show it again after destroy
+        self.clock.pack(side="top") #to keep it at top
     def make_white(self):
 
         self.clock.destroy()
@@ -47,4 +52,4 @@ class GUI:
         self.get_time()
         self.clock.pack(side="top")
 
-GUI()
+GUI()# call the class
